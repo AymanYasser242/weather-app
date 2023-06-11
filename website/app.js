6159d65d5b22655feb02b724aa027dfda4f9eec2
ayman yasser <112979502+AymanYasser242@users.noreply.github.com>
2023-06-11 21:45:44 +0300

    second deploy

--- website/app.js
+++ website/app.js
@@ -1,6 +1,26 @@
 /* Global Variables */
 const apiKey="&appid=f385a981dfee83d52b92320193d2411b&units=metric";
-let baseUrl="https://api.openweathermap.org/data/2.5/forecast?zip=";
+let baseUrl="https://api.openweathermap.org/data/2.5/weather?";
+
+//get current location lon & lat
+let currentLocation = "";
+let latitude = "";
+let longitude = "";
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  currentLocation = `lat=${latitude}&lon=${longitude}`;
+}
+
+
+
 // Create a new date instance dynamically with JS
 let date = new Date();
 let newDate = (date.getMonth()+1)+'.'+ date.getDate()+'.'+ date.getFullYear();
@@ -8,21 +28,23 @@ let newDate = (date.getMonth()+1)+'.'+ date.getDate()+'.'+ date.getFullYear();
 document.getElementById('generator').addEventListener('click',perform);
 //TODO: calling getWeather function to get weather info.
 function perform(){
-    let zip=document.getElementById('zip').value;
+
     let feeling=document.getElementById('feeling').value;
-    getWeather(baseUrl,zip,apiKey)
+    getWeather(baseUrl,currentLocation,apiKey)
     .then(function(data){
     //TODO: sending weather data to the local server.     
-        transData('/add',{date:newDate, city:data.city.name ,temp:data.list[0].main.temp , content:feeling});
+        transData('/add',{date:newDate, city:data.name ,temp:data.main.temp , content:feeling});
     //calling the function to update UI.    
     }).then(function() {
       updateUI()
     });
 };
 
+
+
 //get the weather info  from 'openweather.org' .
-const getWeather = async(url,zip,key)=>{
-const res = await fetch(url+zip+key)
+const getWeather = async(url,currentLocation,key)=>{
+const res = await fetch(url+currentLocation+key)
 try{
     const data= await res.json();
     return data;
@@ -54,9 +76,9 @@ let updateUI = async()=>{
 const request = await fetch('/all');
 try{
 const allData = await request.json();
-document.getElementById('date').innerHTML=`Date: ${allData.date}`;
-document.getElementById('temp').innerHTML=`Temperature: ${(allData.temp)} C°`
-document.getElementById('content').innerHTML=`Feeling: ${allData.content}`
+document.getElementById('date').innerHTML=`Date : ${allData.date}`;
+document.getElementById('temp').innerHTML=`Temperature : ${(allData.temp)} C°`
+document.getElementById('content').innerHTML=`Feeling : ${allData.content}`
 document.getElementById('city').innerHTML=`<h1>${allData.city} <i class="fa-solid fa-earth-americas"></i></h1>`
 }
 catch(error){
